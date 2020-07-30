5eaf5b0e373570cade96212136ae3ecf495dffdc
Evan Hollins <Evan.Hollins@resmed.com.au>
2020-07-30 10:04:59 +1000

    Added more leaves

--- src/App.js
+++ src/App.js
@@ -39,6 +39,43 @@ function App() {
         <Skill title="Skill left" color={Colors.green_dark}/>
         <Skill title="Skill right" color={Colors.purple_dark}/>
       </div>
+      <Branches branches={[
+        {
+          from: {
+            position: 1/4,
+            color: Colors.green_dark
+          },
+          to: {
+            position: 1/6,
+            color: Colors.orange_dark
+          }
+        },
+        {
+          from: {
+            position: 1/4,
+            color: Colors.green_dark
+          },
+          to: {
+            position: 3/6,
+            color: Colors.yellow_dark
+          }
+        },
+        {
+          from: {
+            position: 3/4,
+            color: Colors.purple_dark
+          },
+          to: {
+            position: 5/6,
+            color: Colors.red_dark
+          }
+        },
+      ]}/>
+      <div className="container">
+        <Skill title="Skill one" color={Colors.orange_dark}/>
+        <Skill title="Skill two" color={Colors.yellow_dark}/>
+        <Skill title="Skill three" color={Colors.red_dark}/>
+      </div>
     </div>
   );
 }
